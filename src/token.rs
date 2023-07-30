812399c2ea419f62aa2d7d8d6fe2a75948fdf3d7
EthanDenny <ethandenny@protonmail.com>
2023-07-30 17:54:58 -0230

    Better debugging

--- src/token.rs
+++ src/token.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug, PartialEq)]
 pub enum TokenType {
     LeftParen,
@@ -8,6 +10,19 @@ pub enum TokenType {
     String,
 }
 
+impl fmt::Display for TokenType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            TokenType::LeftParen => write!(f, "("),
+            TokenType::RightParen => write!(f, ")"),
+            TokenType::Atom => write!(f, "Atom"),
+            TokenType::Int => write!(f, "Int"),
+            TokenType::Float => write!(f, "Float"),
+            TokenType::String => write!(f, "String"),
+        }
+    }
+}
+
 pub struct Token<'a> {
     pub token_type: TokenType,
     pub content: &'a str,
@@ -15,17 +30,20 @@ pub struct Token<'a> {
 }
 
 pub fn debug_tokens(tokens: &Vec<Token>) {
-    let mut line = 1;
+    let mut line = 0;
 
     for t in tokens {
         while t.line > line {
-            print!("\n{line:<4}: ");
             line += 1;
+            if line > 1 {
+                println!();
+            }
+            print!("{line:<4}: ");
         }
-        if t.token_type == TokenType::LeftParen || t.token_type == TokenType::RightParen {
-            print!("{} ", t.content);
-        } else {
-            print!("{:?}[{}] ", t.token_type, t.content);
+
+        match t.token_type {
+            TokenType::LeftParen | TokenType::RightParen => print!("{} ", t.content),
+            _ => print!("{}[{}] ", t.token_type, t.content),
         }
     }
 }
