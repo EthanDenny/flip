7e95cf0dddd21eb95122f7d21d9e23a48f476053
Ethan Denny <ethandenny@protonmail.com>
2024-03-03 16:06:32 -0330

    Add lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+pub mod ast;
+pub mod compiler;
+pub mod error;
+pub mod parser;
+pub mod scanner;
+pub mod symbols;
+pub mod tokens;
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
-mod ast;
-mod compiler;
-mod error;
-mod parser;
-mod scanner;
-mod symbols;
-mod tokens;
-
 use std::env;
 use std::fs::{self, File};
 use std::io::prelude::*;
 use std::path::PathBuf;
+
+use flip::{compiler, parser, scanner};
 
 pub fn main() {
     let args: Vec<String> = env::args().collect();
@@ -17,7 +11,7 @@ pub fn main() {
     if args.len() == 2 {
         let path = PathBuf::from(&args[1]);
         let code = fs::read_to_string(&path).expect("Could not read file");
-        
+
         if let Err(e) = compile(code) {
             eprintln!("{e}");
         };
@@ -26,7 +20,7 @@ pub fn main() {
     }
 }
 
-fn compile(code: String) -> std::io::Result<()>  {
+fn compile(code: String) -> std::io::Result<()> {
     let mut symbols = compiler::table_from_inlines();
     let tokens = scanner::get_tokens(&code);
 
@@ -37,7 +31,7 @@ fn compile(code: String) -> std::io::Result<()>  {
     file.write_all(format!("{INCLUDES}\n\n").as_bytes())?;
 
     for branch in ast {
-        let buf = compiler::compile_expr(&branch, &mut symbols);
+        let buf = flip::compiler::compile_expr(&branch, &mut symbols);
         file.write_all(buf.get().as_bytes())?;
     }
 
